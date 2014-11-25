f4bb0165874dad486f23f02bdaa1b2eda693ed98
Jean Roussel <contact@jean-roussel.fr>
2014-11-25 20:15:11 +0100

    Customized theme

--- wp-content/themes/theme43375/static/static-twitter.php
+++ wp-content/themes/theme43375/static/static-twitter.php
@@ -2,4 +2,7 @@
 	<div id="twitter_h">
 		<a href="http://www.sol-eloc.info/" target="_blank">&nbsp;</a>
 	</div>
+	<div style="margin:0px 20px;background: url('/wp-content/themes/logo-cnlrq.gif') no-repeat scroll 0 0 rgba(0, 0, 0, 0);color: #9a9e9f;float: right;font-size: 17px;height: 87px;line-height: 17px;width: 293px;">
+		<a href="http://www.cnlrq.org/" target="_blank" style="display:block;width:293px;height:87px;">&nbsp;</a>
+	</div>
 <?php } ?>
